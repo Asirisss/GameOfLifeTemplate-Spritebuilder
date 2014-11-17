2ccd158bcf50df1302cc8b52e839e73b4ff268c8
Asirisss <asirisss87@gmail.com>
2014-11-18 00:12:05 +0600

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -29,42 +29,44 @@ static const int GRID_COLUMNS = 10;
     self.userInteractionEnabled = YES;
 }
 
-- (void)setupGrid
+-(void)setupGrid
 {
-    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
+    //divide that grid's size by the number of collums/rows to figure the right width and height of each cell
     _cellWidth = self.contentSize.width / GRID_COLUMNS;
     _cellHeight = self.contentSize.height / GRID_ROWS;
     
     float x = 0;
     float y = 0;
     
-    // initialize the array as a blank NSMutableArray
+    //initialize the array as a blank NSutableArray
     _gridArray = [NSMutableArray array];
     
-    // initialize Creatures
+    //initialize Creatures
     for (int i = 0; i < GRID_ROWS; i++) {
         // this is how you create two dimensional arrays in Objective-C. You put arrays into arrays.
         _gridArray[i] = [NSMutableArray array];
         x = 0;
         
         for (int j = 0; j < GRID_COLUMNS; j++) {
-            Creature *creature = [[Creature alloc] initCreature];
-            creature.anchorPoint = ccp(0, 0);
+            Creature *creature = [[Creature alloc]initCreature];
+            creature.anchorPoint = ccp(0,0);
             creature.position = ccp(x, y);
             [self addChild:creature];
             
-            // this is shorthand to access an array inside an array
+            //this is shorthand to access an array inside an array
             _gridArray[i][j] = creature;
             
-            // make creatures visible to test this method, remove this once we know we have filled the grid properly
-            creature.isAlive = YES;
+            /* make creatures visible to test this method, remove this once we know we have filled the grid properly
+            creature.isAlive = YES;*/
             
             x+=_cellWidth;
+
         }
-        
-        y += _cellHeight;
+        y+=_cellHeight;
+
     }
 }
+
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     //get the x,y coordinates of the touch
